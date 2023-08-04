0d603aaa9bd60026e35f37bcfff936b20e04c751
Yahya Unlu <yhunlu87@gmail.com>
2023-08-04 17:29:43 +0300

    done react-blog-boilerplate for microservices.

---
+++ client/src/CommentList.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+const CommentList = ({ postId }) => {
+  const [comments, setComments] = useState([]);
+
+  const fetchData = async () => {
+    const res = await axios.get(
+      `http://localhost:4001/posts/${postId}/comments`
+    );
+
+    setComments(res.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const renderedComments = comments.map((comment) => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+  return <ul>{renderedComments}</ul>;
+};
+
+export default CommentList;
