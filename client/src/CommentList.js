8c819004e94d416b15e5ed574761d3dbb0d195a5
yhunlu <yhunlu87@gmail.com>
2023-08-06 18:30:25 +0300

    refactoring components in order to implement microservices.

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,21 +1,6 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  const fetchData = async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-
-    setComments(res.data);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
+const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
     return <li key={comment.id}>{comment.content}</li>;
   });
