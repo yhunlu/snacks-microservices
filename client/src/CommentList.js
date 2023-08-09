75acaf6377a527c674263da33f95bce2104d1e21
yhunlu <yhunlu87@gmail.com>
2023-08-10 01:39:31 +0300

    rendering comment status in client side.

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    return (
+      <li key={comment.id}>
+        {comment.content} - {comment.status}
+      </li>
+    );
   });
   return <ul>{renderedComments}</ul>;
 };
