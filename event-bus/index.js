2cfdef42509e5190dd78b8cd86bdb5b29b9751fe
Yahya Unlu <yhunlu87@gmail.com>
2023-08-19 00:02:35 +0300

    refactoring URL for clusterip, create docker images-push dockerhub, create deployment-clusterip service for eachone, add new clusterip URL to event-bus.

--- event-bus/index.js
+++ event-bus/index.js
@@ -13,18 +13,22 @@ app.post('/events', (req, res) => {
 
     events.push(event);
 
-    axios.post('http://localhost:4000/events', event).catch((err) => {
+    axios.post('http://posts-clusterip-srv:4000/events', event).catch((err) => {
       console.log(err.message);
     });
-    axios.post('http://localhost:4001/events', event).catch((err) => {
+    axios
+      .post('http://comments-clusterip-srv:4001/events', event)
+      .catch((err) => {
+        console.log(err.message);
+      });
+    axios.post('http://query-clusterip-srv:4002/events', event).catch((err) => {
       console.log(err.message);
     });
-    axios.post('http://localhost:4002/events', event).catch((err) => {
-      console.log(err.message);
-    });
-    axios.post('http://localhost:4003/events', event).catch((err) => {
-      console.log(err.message);
-    });
+    axios
+      .post('http://moderation-clusterip-srv:4003/events', event)
+      .catch((err) => {
+        console.log(err.message);
+      });
 
     res.send({ status: 'OK' });
   } catch (error) {
